a30431f7f92c51bf6237e8df89dfdb3c97a66569
graday2 <aoife.grady2@mail.dcu.ie>
2021-11-29 17:39:33 +0000

    Load sound
    - Play original sound
    - Plot time-frequency response
    - Play filtered signal

---
+++ filterSound/sigprocMXC_filterGlass.m
@@ -0,0 +1,78 @@
+%%
+%     COURSE: Signal processing problems, solved in MATLAB and Python
+%    SECTION: Introduction
+%      VIDEO: Having fun with filtered Glass dance
+% Instructor: sincxpress.com
+%
+%%
+
+% load the file
+load glassDance.mat
+% this is a clip of Philip Glass, Dance VII (https://www.youtube.com/watch?v=LpewOlR-z_4)
+
+
+% play the music!
+soundsc(glassclip,srate) 
+%srate 44.1 kHz which is typical sampling rate for auditory streams
+
+% some variables for convenience
+pnts = length(glassclip);
+timevec = (0:pnts-1)/srate;
+
+% draw the time-domain signals
+figure(1), clf
+subplot(511)
+plot(timevec,glassclip)
+xlabel('Time (s)')
+
+%shows left and right speaker channels so what the left and right ear hears
+
+
+%% static power spectrum and pick a frequency range
+
+% inspect the power spectrum
+hz = linspace(0,srate/2,floor(length(glassclip)/2)+1);
+powr = abs(fft(glassclip(:,1))/pnts);
+
+subplot(512), cla
+plot(hz,powr(1:length(hz)))
+set(gca,'xlim',[100 2000],'ylim',[0 max(powr)])
+xlabel('Frequency (Hz)'), ylabel('Amplitude')
+
+
+% pick frequencies to filter
+frange = [  300  460 ];
+frange = [ 1000 1100 ];
+% frange = [ 1200 1450 ];
+
+
+% design an FIR1 filter
+fkern = fir1(2001,frange/(srate/2),'bandpass');
+
+% apply the filter to the signal
+filtglass(:,1) = filtfilt(fkern,1,glassclip(:,1));
+filtglass(:,2) = filtfilt(fkern,1,glassclip(:,2));
+
+% plot the filtered signal power spectrum
+hold on
+powr = abs(fft(filtglass(:,1))/pnts);
+plot(hz,powr(1:length(hz)),'r')
+
+
+% plot the time-frequency response (also known as the spectrogram)
+%Each one of the little red blobs corresponds to a concentration of energy 
+%at a particular frequency at a particular time - and that each of the
+%keystrokes on the piano
+subplot(5,1,3:5)
+spectrogram(glassclip(:,1),hann(round(srate/10)),[],[],srate,'yaxis');
+hold on
+plot(timevec([1 1; end end]),frange([1 2; 1 2])/1000,'k:','linew',2)
+
+set(gca,'ylim',[0 2])
+
+%% play the sound
+
+soundsc(filtglass,srate)
+
+%% done.
+
